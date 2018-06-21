a21d25502449beb5faffb06bf26ff6e2bcf49eea
Beni Egli <beni.egli90@gmail.com>
2018-06-21 19:04:52 +0200

    Player class und grobkonzept erstellt

---
+++ Soccer_Manager/Soccer_Manager/Model/Enums.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soccer_Manager.Model
+{
+    public class Enums
+    {
+        enum Position
+        {
+            Keeper,
+            Defender,
+            Middlefield,
+            Striker
+        };
+
+        enum Nationality
+        {
+            Schweiz,
+            Frankreich,
+            England
+        }
+
+
+
+    }
+}
---
+++ Soccer_Manager/Soccer_Manager/Model/Player.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soccer_Manager.Model
+{
+    public class Player
+    {
+        public string Lastname { get; set; }
+        public string Firstname { get; set; }
+        public decimal Heigth { get; set; }
+        public DateTime Birthdate { get; set; }
+        public int Jerseynumber { get; set; }
+        public enum  Position  { get; set; }
+        public int PlayedGames { get; set; }
+        public int Goals { get; set; }
+        public enum Nationality { get; set; }
+
+
+
+
+
+    }
+}
