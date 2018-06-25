789c0943d12d6de26c268e19f096a5e57efad025
Beni Egli <beni.egli90@gmail.com>
2018-06-25 17:39:25 +0200

    SQL DB erstellen und CREATE und DROP Table erstellen

---
+++ 01-CreateTables.sql
@@ -0,0 +1,69 @@
+-- CreateTables.sql
+--
+-- Autor		: Beni Egli
+-- Projekt		: HFU
+-- Version		: 1.0
+-- 
+-- Change log
+-- 21/06/2018	BEGLI	Erstellt
+-- 
+
+-- DB anwählen
+use Soccer;
+
+-- Tabellen erstellen (Step 1)
+
+-- Tabelle Position erstellen
+create table Position
+(
+	ID		int		identity(1,1)	not null,
+	Position	varchar(20)				null
+	CONSTRAINT PK_Position PRIMARY KEY (ID) 
+);
+
+-- Tabelle Team erstellen
+create table Team
+(
+	ID			int	identity(1,1)	not null,
+	Team			varchar (80)		null,
+	Pictureflag		image				null
+	CONSTRAINT PK_Team PRIMARY KEY (ID) 
+);
+
+-- Tabelle Player erstellen
+create table Player
+(
+	ID			int	identity(1,1)	not null,
+	Lastname			varchar (80)	null,
+	Firstname			varchar(80)		null,
+	Height				varchar (80)	null,
+	Birthdate			date			null,
+	Jerseynumber		int				null,
+	PositionID			int				null,
+	Playedgames			int				null,
+	Goals				int				null,
+	Picture				image			null,
+	TeamID				int				null
+	CONSTRAINT PK_Player PRIMARY KEY (ID) 
+);
+
+
+-- ändern der Tabelle bzw. ergänzen mit Foreign Keys
+
+alter table Player
+add CONSTRAINT FK_Player_Position FOREIGN KEY (PositionID)
+	REFERENCES Position(ID),
+	CONSTRAINT FK_Player_Team FOREIGN KEY (TeamID)
+	REFERENCES Team(ID)
+go
+
+-- ergänzen mit Unique constraints
+alter Table Team
+	add constraint uq_team unique(Team);
+
+alter Table Position
+	add constraint uq_position unique(Position);
+
+go
+
+
---
+++ 04-DropTables.sql
@@ -0,0 +1,18 @@
+-- DropAboTables.sql
+--
+-- Autor		: Beni Egli
+-- Projekt		: HBU
+-- Version		: 1.0
+-- 
+-- Change log
+-- 21/09/2017	BEGLI	Erstellt
+-- 
+
+-- DB anwählen
+use Soccer;
+
+drop table if exists Player
+drop table if exists Team
+drop table if exists Position
+
+go
